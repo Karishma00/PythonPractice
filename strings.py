9a161b37c22dfd98143fde273983e92995a55d61
Karishma00 <55571412+Karishma00@users.noreply.github.com>
2019-09-20 18:24:10 +0530

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,33 @@
+str="Company"
+s="K"
+print(str)
+print(str[2])
+
+#indexing
+print(str[-1])
+
+#Slicing
+print(str[1:6:2])
+print(str[:2])
+print(str[:2:-1])
+
+#str[0]='K'
+#print(str)
+
+print(s+str)
+print(str*2)
+
+#method of strings
+k='Ka ris hma Sala liya'
+print(k.upper())
+
+print(k.lower())
+
+print(k.split())
+print(k.split('a'))
+print('Chips')
+
+#String formatting
+
+
+
